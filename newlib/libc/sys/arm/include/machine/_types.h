6c444abfd1f1578d089a03eea758afff478d153e
Dave Murphy <davem@devkitpro.org>
2016-11-02 23:34:19 +0000

    __dev_t & __ino_t need to be 32bit

--- newlib/libc/sys/arm/include/machine/_types.h
+++ newlib/libc/sys/arm/include/machine/_types.h
@@ -10,5 +10,10 @@ typedef __int64_t _off_t;
 typedef __int64_t _fpos_t;
 #define __machine_fpos_t_defined
 
+typedef __uint32_t __ino_t;
+#define __machine_ino_t_defined
+
+typedef __uint32_t __dev_t;
+#define __machine_dev_t_defined
 #endif
 
